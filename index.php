5251915721973a1f0699e03f7432366712f28d5c
Hani <haniabukhait@gmail.com>
2019-09-02 19:52:58 +0300

    index page

--- index.php
+++ index.php
@@ -1,33 +1,88 @@
-<html>
-    <head>
-        <title>CRUD: CReate, Update, Delete PHP MySQL</title>
-    </head>
-    <body>
-        <form method="post" action="server.php" >
-            <div >
-                <label type="text" name="id" value="">id</label>
-             
-            </div>
-            <div class="input-group">
-                <label>title</label>
-                <input type="text" name="title" value="">
-            </div>
-            <div class="input-group">
-                <label>tag</label>
-                <input type="text" name="tag" value="">
-            </div>
-            <div class="input-group">
-                <label>author</label>
-                <input type="text" name="author" value="">
-            </div>
-            <div class="input-group">
-                <label>action</label>
-                <input type="text" name="action" value="">
-            </div>
+<!DOCTYPE html>
+<html lang="en">
 
-            <div class="input-group">
-                <button class="btn" type="submit" name="save" >Save</button>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+
+<body>
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">Books</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contact</a>
+                </li>
+            </ul>
+            <!-- <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-info my-2 my-sm-0" type="submit">Search</button>
+            </form> -->
+        </div>
+    </nav>
+
+
+    <!-- Table -->
+    <div class="container mt-4">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="jumbotron">
+                    <h4 class="mb-4">All Books</h4>
+
+                    <table class="table table-dark">
+                        <thead>
+                            <tr>
+                                <th scope="col">ID</th>
+                                <th scope="col">Title</th>
+                                <th scope="col">Tag</th>
+                                <th scope="col">Auther</th>
+                                <th scope="col">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>Book1</td>
+                                <td>PHP</td>
+                                <td>Micheal</td>
+                                <td>add/edit/delete</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">2</th>
+                                <td>Book2</td>
+                                <td>JavaScript</td>
+                                <td>Johnson</td>
+                                <td>add/edit/delete</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">3</th>
+                                <td>Book3</td>
+                                <td>Java</td>
+                                <td>John</td>
+                                <td>add/edit/delete</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
-        </form>
-    </body>
+        </div>
+    </div>
+
+</body>
+
 </html>
