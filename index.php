c48b0c92488f9082162b17b774262bb51befad16
Hani <haniabukhait@gmail.com>
2019-09-02 23:37:01 +0300

    small fixes

--- index.php
+++ index.php
@@ -33,9 +33,6 @@ require_once 'models/books.php';
                 <li class="nav-item">
                     <a class="nav-link" href="#">About</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Contact</a>
-                </li>
             </ul>
             <!-- <form class="form-inline my-2 my-lg-0">
                 <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
