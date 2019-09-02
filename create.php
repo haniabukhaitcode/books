2fd76034bf27adb3d52691d77fe85036828c22a2
Hani <haniabukhait@gmail.com>
2019-09-03 01:32:05 +0300

    insert is done need button that goes to path /create.

--- create.php
+++ create.php
@@ -1,102 +1,92 @@
+<?php
+
+require_once 'config/database.php';
+require_once 'models/books.php';
+
+if (isset($_POST['submit'])) {
+
+    $title = $_POST['title'];
+    $tag = $_POST['tag'];
+    $author = $_POST['author'];
+
+
+    $fields = [
+        'title' => $title,
+        'tag' => $tag,
+        'author' => $author
+    ];
+
+    $books = new books();
+    $books->insert($fields);
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>CRUD in PHP</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
+
 <body>
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">Books</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-    <div class="container">
-        <div class="page-header">
-            <h1>Create Book</h1>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">About</a>
+                </li>
+            </ul>
+            <!-- <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-info my-2 my-sm-0" type="submit">Search</button>
+            </form> -->
         </div>
-
-    <?php 
-    if($_POST){
-try{
-        // insert query
-        $query= "INSERT INTO booksTable  SET title=:title, tag=:tag, author=:author, action=:action";
-        
-        //prepare query for execution
-        $stmt = $con->prepare($query);
-
-        //posted values
-        $title=htmlspecialchars(script_tags($_POST['title']));
-        $tag=htmlspecialchars(script_tags($_POST['tag']));
-        $author=htmlspecialchars(script_tags($_POST['author']));
-        $action=htmlspecialchars(script_tags($_POST['action']));
-
-        //bind the parameters
-        $stmt->bindParam(':title',$title);
-        $stmt->bindParam(':tag',$tag);
-        $stmt->bindParam(':author',$author);
-        $stmt->bindParam(':action',$action);
-
-        //execute the query
-        if($stmt->execute()){
-            echo "<div class='alert alert-success'>Record was saved.</div>";
-        }else{
-            echo "<div class='alert alert-danger'>Unable to save record.</div>";
-        }
+    </nav>
 
 
-}catch(PDOException $exception){
-    die("Error " . $exception->getMessage());
-}
+    <!-- Table -->
+    <div class="container mt-4">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="jumbotron">
+                    <h4 class="mb-4">Add Books</h4>
 
-    }
-?>
-
-
- <!-- html form to create product will be here -->
- <!-- The $_SERVER["PHP_SELF"] is a super global variable that returns the filename of the currently executing script. -->
- <form acion ="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
-    <table class="table table-hove table-resposive table-bordered">
-        <tr>
-            <td>Title</td>
-            <td><input type="text" name="title" class="form-control" /></td>
-        </tr>
-
-        <tr>
-            <td>tag</td>
-            <td><input type="text" name="tag" class="form-control" /></td>
-        </tr>
-
-        <tr>
-            <td>Author</td>
-            <td><input type="text" name="author" class="form-control" /></td>
-        </tr>
-
-        <tr>
-            <td>Action</td>
-            <td><input type="text" name="action" class="form-control" /></td>
-        </tr>
-
-        <tr>
-            <td></td>
-            <td>
-                <input type='submit' value='Save' class='btn btn-primary' />
-                <a href='index.php' class='btn btn-danger'>Back to read products</a>
-            </td>
-        </tr>
-
-    </table>
+                    <form action="" method="post">
+                        <div class="form-group">
+                            <label for="title">Title</label>
+                            <input type="text" name="title" class="form-control" aria-describedby="emailHelp" placeholder="Enter book name">
+                        </div>
+                        <div class="form-group">
+                            <label for="tag">Tag</label>
+                            <input type="text" name="tag" class="form-control" placeholder="Tag">
+                        </div>
+                        <div class="form-group">
+                            <label for="author">Author:</label>
+                            <input type="text" name="author" class="form-control" placeholder="Author name">
+                        </div>
 
 
-</form>
+                        <input type="submit" name="submit" class="btn btn-primary" />
+                    </form>
 
-        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-        <script
-  src="https://code.jquery.com/jquery-3.4.1.min.js"
-  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
-  crossorigin="anonymous"></script>
- 
-   
-<!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 </body>
+
 </html>
--- models/books.php
+++ models/books.php
@@ -14,4 +14,26 @@ class books extends database
             return $data;
         }
     }
+    public function insert($fields)
+    {
+        //implode is a php method joins array elements with a string 
+        $implodeColumns = implode(', ', array_keys($fields));
+
+        $implodePlaceholder = implode(", :", array_keys($fields));
+
+        //INSERT INTO booksTable (title, tag, author) VALUES(:title, :tag, :author)
+        $sql = "INSERT INTO booksTable ($implodeColumns) VALUES(:" . $implodePlaceholder . ")";
+
+        $stmt = $this->connect()->prepare($sql);
+
+        foreach ($fields as $key => $value) {
+            $stmt->bindValue(":" . $key, $value);
+        }
+
+        $stmtExec = $stmt->execute();
+
+        if ($stmtExec) {
+            header('Location: index.php');
+        }
+    }
 }
