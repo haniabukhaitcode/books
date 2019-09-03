5564df9ad6beb9121c3dcc66763f67cde4473ff8
Hani <haniabukhait@gmail.com>
2019-09-04 00:50:56 +0300

    added insert button on index.php

--- index.php
+++ index.php
@@ -52,8 +52,10 @@ if (isset($_GET['del'])) {
         <div class="row">
             <div class="col-lg-12">
                 <div class="jumbotron">
-                    <h4 class="mb-4">All Books</h4>
-
+                    <div class="row">
+                        <h4 class="col-12 mb-3">All Books</h4>
+                        <a type="submit" class="btn btn-success col-2 mb-4 ml-3 p-1" href="create.php">Insert a book</a>
+                    </div>
                     <table class="table table-dark">
                         <thead>
                             <tr>
